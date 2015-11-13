dc6eac1a92ffe1a233a99e53b4b621177f6d6ccc
Malena <malenacorl@gmail.com>
2015-11-13 11:16:07 +0000

    Exercises 5.6

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -9,5 +9,9 @@ test "layout links" do
   assert_select "a[href=?]", help_path
   assert_select "a[href=?]", about_path
   assert_select "a[href=?]", contact_path
+  get signup_path
+  assert_select "title", full_title("Sign up")
 end
+
+
 end
