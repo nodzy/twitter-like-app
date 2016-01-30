a4d1065d3263df45a8cfe7d9133301395f3930f5
Malena <malenacorl@gmail.com>
2016-01-30 15:32:55 +0000

    Exercise 9.2

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -2,12 +2,28 @@ require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
 
+
+def setup
+  @user = users(:michael)
+end
+
 test "layout links" do
-  get root_path
+   get root_path
   assert_template 'static_pages/home'
   assert_select "a[href=?]", root_path, count: 2
   assert_select "a[href=?]", help_path
   assert_select "a[href=?]", about_path
   assert_select "a[href=?]", contact_path
+   assert_select "a[href=?]", logout_path, count: 0
+    assert_select "a[href=?]", login_path, count: 1
+   log_in_as(@user)
+   get user_path(@user)
+    assert_select "a[href=?]", logout_path, count: 1
+    assert_select "a[href=?]", login_path, count: 0
+  
+ 
+  
+  
+  
 end
 end
