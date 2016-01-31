73f6cf73a92981e2e660464ff4ef3295d30f519b
Malena <malenacorl@gmail.com>
2016-01-31 12:50:15 +0000

    Change CarrierWave configuration again

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -8,8 +8,7 @@ config.fog_credentials = {
     :s3_access_key_id      => ENV['S3_ACCESS_KEY'],                        # required
     :s3_secret_access_key  => ENV['S3_SECRET_KEY'],                     # required
     :region                 => 'eu-west-1',                  # optional, defaults to 'us-east-1'
-    :host                   => 's3.example.com',             # optional, defaults to nil
-    :endpoint               => 'https://s3.example.com:8080' # optional, defaults to nil
+     # optional, defaults to nil
 }
 
 config.fog_directory = ENV['S3_BUCKET']
