d8146d02ddc7d5060661f2b51810d188c63300fe
Malena <malenacorl@gmail.com>
2016-01-31 08:10:41 +0000

    Exercise 10.3

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,8 +21,8 @@ end
 
 
 def activate
-update_attribute(:activated, true)
-update_attribute(:activated_at, Time.zone.now)
+
+update_columns(activated:true, activated_at: Time.zone.now)
 end
 
 def send_activation_email
@@ -32,8 +32,9 @@ end
 
 def create_reset_digest
 self.reset_token = User.new_token
-update_attribute(:reset_digest, User.digest(reset_token))
-update_attribute(:reset_sent_at, Time.zone.now)
+
+update_columns(reset_digest: User.digest(reset_token), reset_sent_at: Time.zone.now)
+
 end
 # Sends password reset email.
 def send_password_reset_email
